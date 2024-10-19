5c8a4f16440e82745455ee645487ebf94d7951bf
Daniel Krucso <your-new-email@example.com>
2024-10-19 09:29:43 +0200

    feature: Finish Search page

--- src/app/pages/search-page/search-page.component.ts
+++ src/app/pages/search-page/search-page.component.ts
@@ -1,4 +1,8 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { selectAllBooks, selectLoading } from '../../../shared/store/book.selectors';
+import { Observable } from 'rxjs';
+import { IBook } from '../../../shared/models/book.model';
 
 @Component({
   selector: 'app-search-page',
@@ -6,6 +10,12 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrl: './search-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SearchPageComponent {
+export class SearchPageComponent  {
+  books$: Observable<IBook[] | undefined>
+  isLoading$!: Observable<boolean>;
 
+  constructor(private store: Store) {
+    this.books$ = this.store.select(selectAllBooks);
+    this.isLoading$ = this.store.select(selectLoading);
+  }
 }
